07054f3170ae719cce0a37587f948dd8fb97d881
Runbin Shi <runbin.shi@inf.ethz.ch>
2023-01-09 11:50:20 +0100

    Fix ci

--- run.sh
+++ run.sh
@@ -13,7 +13,7 @@ fi
 ./mill version
 
 #./mill hwsys.test
-mill hwsys.test.testSim dedup.Sha3Tests
+./mill hwsys.test.testSim dedup.Sha3Tests
 #FIXME: CRCTests sometimes fail due to the sw reference issue
 # mill hwsys.test.testSim dedup.CRCTests
 
